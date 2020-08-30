297d2c78e67831db9e779a7445d0b36356fe2013
krishna <krishna@pl.com>
2020-08-30 20:46:41 +0530

    Updates

---
+++ precache-manifest.830d521c7b2fcf350fa12e8e473811eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f560d35edbf5a8bbd091d3d3700d7b45",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "597e0468cfbff75ad81a",
+    "url": "/todo/static/css/main.8aeb7856.chunk.css"
+  },
+  {
+    "revision": "3b6c9a8c6957f9d0f8b7",
+    "url": "/todo/static/js/2.75f37360.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.75f37360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597e0468cfbff75ad81a",
+    "url": "/todo/static/js/main.a36b6031.chunk.js"
+  },
+  {
+    "revision": "a03a00bd26cf7edb86b6",
+    "url": "/todo/static/js/runtime-main.8eb62d04.js"
+  }
+]);
